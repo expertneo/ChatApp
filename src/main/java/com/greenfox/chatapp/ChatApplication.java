7a884fb515c688811e40c1cb4cf65477e714a69d
Bence Papp <bence.papp.hu@gmail.com>
2018-01-29 13:55:15 +0100

    heroku commit

--- src/main/java/com/greenfox/chatapp/ChatApplication.java
+++ src/main/java/com/greenfox/chatapp/ChatApplication.java
@@ -1,12 +1,18 @@
 package com.greenfox.chatapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatApplication {
+public class ChatApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 }
---
+++ src/main/java/com/greenfox/chatapp/controllers/WebController.java
@@ -0,0 +1,13 @@
+package com.greenfox.chatapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+  @GetMapping("/")
+public String mainPage() {
+  return "main";
+}
+}
